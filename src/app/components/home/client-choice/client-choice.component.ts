d9a195b858ab481486e8ab27a51d939baea0719d
mohammed Attieah <77529076+mohammedMamdouh93@users.noreply.github.com>
2025-11-25 01:29:46 +0200

    fix issue in design

--- src/app/components/home/client-choice/client-choice.component.ts
+++ src/app/components/home/client-choice/client-choice.component.ts
@@ -1,13 +1,36 @@
+import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-client-choice',
+  imports: [NgClass],
   templateUrl: './client-choice.component.html',
   styleUrls: ['./client-choice.component.css'],
 })
 export class ClientChoiceComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  zoomLevel = 100;
+  currentZoom = 100;
+  private zoomInterval: any;
+
+  ngOnInit(): void {
+    this.startZoomWatcher();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.zoomInterval);
+  }
+
+  startZoomWatcher() {
+    this.zoomInterval = setInterval(() => {
+      const currentZoom = Math.round(window.devicePixelRatio * 100);
+      if (currentZoom >= 250) {
+        this.currentZoom = currentZoom;
+      } else {
+        this.currentZoom = 100;
+      }
+    }, 200); // checks 5 times per second (lightweight)
+  }
 }
